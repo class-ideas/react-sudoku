914ea1ce7857582b1ee53f79e3cc11d2d5a2a70d
JD Isaacks <jd@jisaacks.com>
2016-05-07 08:37:18 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
 // Javascript Entry Point
-
-import React from 'react';
-import { render } from 'react-dom';
-
-import Game from './components/game';
-
-render((
-  <Game/>
-), document.querySelector('.app'))
